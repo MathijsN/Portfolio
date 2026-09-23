497aa15bccebd4b526d5f34fc6d471979d7ebfca
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-09-23 20:25:53 +0200

    setup

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<article>
+    <h2>Ttiel</h2>
+    <p>met wat tekst</p>
+
+    <ul>
+        <li>labels</li>
+        <li>label</li>
+        <li>label</li>
+    </ul>
+</article>
+
+<style></style>
